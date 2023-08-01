44bcc49446454e00c73910864e408eb784fe0aa5
Sameer Dey <kalantrilonda@gmail.com>
2023-08-01 07:32:55 +0530

    Webcam Access Part 1 Completed

--- main.py
+++ main.py
@@ -1,2 +1,20 @@
-print("hello world")
+import cv2
+
+
+capture = cv2.VideoCapture(0)
+capture.set(3,1920)
+capture.set(4,1080)
+
+
+while True:
+    success, img = capture.read()
+    cv2.imshow("Face Attadance ", img)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+   
+capture.release()
+cv2.destroyAllWindows()
+
+
+
 
