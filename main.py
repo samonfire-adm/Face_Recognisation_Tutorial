cb7d3b0b6f766e2b3b008330471606711bebc0e5
Sameer Dey <kalantrilonda@gmail.com>
2023-08-11 17:25:04 +0530

    Some changes made

--- main.py
+++ main.py
@@ -5,10 +5,13 @@ capture = cv2.VideoCapture(0)
 capture.set(3,1920)
 capture.set(4,1080)
 
+bg = cv2.imread('Source/bg.png')
 
 while True:
     success, img = capture.read()
-    cv2.imshow("Face Attadance ", img)
+    # cv2.imshow("Face Attadance ", img)
+    bg[162:162+480, 55:55+640] = img
+    cv2.imshow("Main", bg)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
    
